68c8e44753557254fa4e3d81dc95f1c85945ef93
vikram221969 <narojuvikram2003@gmail.com>
2024-11-30 15:38:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function validate()
-{
-    let n=document.getElementById("name").value;
-    if(n!="")
-    {
-    document.getElementById("display").textContent="HELLO:"+n;
-}
-else
-{
-    document.getElementById("string").textContent="matter enter cheyyi bro";
-}
-}
+
